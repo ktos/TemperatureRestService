af12e98c7da91a21a9477e2d33d43233a023bd94
Marcin Badurowicz <m@badurowicz.net>
2016-03-14 18:27:41 +0100

    minor details

--- views/data-wns.html.php
+++ views/data-wns.html.php
@@ -1,17 +1,18 @@
 <tile>
-  <visual branding="logo">
+  <visual>
 
     <binding template="TileSmall" hint-textStacking="center">
       <text hint-style="base" hint-align="center"><?= round((float)$data, 1);?>°C</text>
+	  <text hint-style="captionSubtle"><?= $name ?></text>
     </binding>
 
-    <binding template="TileMedium">
-      <text hint-style="title"><?= round((float)$data, 1);?>°C</text>
+    <binding template="TileMedium" branding="logo">
+      <text hint-style="subtitle"><?= round((float)$data, 1);?>°C</text>
       <text hint-style="bodySubtle"><?= $name ?></text>
     </binding>
 
-    <binding template="TileWide">
-      <text hint-style="title"><?= round((float)$data, 1);?>°C</text>
+    <binding template="TileWide" branding="logo">
+      <text hint-style="subtitle"><?= round((float)$data, 1);?>°C</text>
       <text hint-style="bodySubtle"><?= $name ?></text>
       <text hint-style="captionSubtle">last update: <?= date('Y-m-d H:i:s', $lastupdated); ?></text>
     </binding>
