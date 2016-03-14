b7da9c3e1bf3e0387e9ddef198c7ac2e7aa242aa
Marcin Badurowicz <m@badurowicz.net>
2016-03-14 18:18:21 +0100

    update for Windows 10

--- views/data-wns.html.php
+++ views/data-wns.html.php
@@ -1,8 +1,20 @@
 <tile>
-	<visual>
-		<binding template="TileSquareText01">
-			<text id="1"><?= round((float)$data, 1);?> °C</text>
-			<text id="2"><?= $name ?></text>
-		</binding>
-	</visual>
+  <visual branding="logo">
+
+    <binding template="TileSmall" hint-textStacking="center">
+      <text hint-style="base" hint-align="center"><?= round((float)$data, 1);?>°C</text>
+    </binding>
+
+    <binding template="TileMedium">
+      <text hint-style="title"><?= round((float)$data, 1);?>°C</text>
+      <text hint-style="bodySubtle"><?= $name ?></text>
+    </binding>
+
+    <binding template="TileWide">
+      <text hint-style="title"><?= round((float)$data, 1);?>°C</text>
+      <text hint-style="bodySubtle"><?= $name ?></text>
+      <text hint-style="captionSubtle">last update: <?= date('Y-m-d H:i:s', $lastupdated); ?></text>
+    </binding>
+
+  </visual>
 </tile>
